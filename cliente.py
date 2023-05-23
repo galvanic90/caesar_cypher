6e21e09436bdf426db4571c6000d02cc5ff01c41
Jhon Alejandro Garcia Pareja <jhon.garciap@udea.edu.co>
2023-05-22 19:05:47 -0500

    cambios

--- cliente.py
+++ cliente.py
@@ -1,10 +1,17 @@
 import socket
+from cifrado import cifrado_cesar
+
+texto_original = input('Ingrese el texto a cifrar: ')
+clave = 4
+texto_cifrado = cifrado_cesar(texto_original, clave)
+print("Texto original:", texto_original)
+print("Texto cifrado:", texto_cifrado)
 
 # El cliente debe tener las mismas especificaciones del servidor
 host = socket.gethostname()
 port = 3000
 BUFFER_SIZE = 1024
-MESSAGE = 'Hola, mundo!' # Datos que queremos enviar
+MESSAGE = texto_cifrado # Datos que queremos enviar
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_tcp:
     socket_tcp.connect((host, port))
--- main.py
+++ main.py
@@ -1,31 +1,4 @@
-def cifrado_cesar(texto, clave):
-    resultado = ''
-    for caracter in texto:
-        if caracter.isalpha():
-            # Obtener el código ASCII del caracter y calcular la clave
-            codigo = ord(caracter)
-            codigo_desplazado = (codigo - ord('a') + clave) % 26 + ord('a')
-            # Convertir el código desplazado de vuelta a caracter
-            caracter_desplazado = chr(codigo_desplazado)
-            resultado += caracter_desplazado
-        else:
-            resultado += caracter
-    return resultado
-
-
-def descifrado_cesar(texto_cifrado, clave):
-    resultado = ''
-    for caracter in texto_cifrado:
-        if caracter.isalpha():
-            # Obtener el código ASCII del caracter y calcular la clave inverso
-            codigo = ord(caracter)
-            codigo_desplazado = (codigo - ord('a') - clave) % 26 + ord('a')
-            # Convertir el código desplazado inverso de vuelta a caracter
-            caracter_desplazado = chr(codigo_desplazado)
-            resultado += caracter_desplazado
-        else:
-            resultado += caracter
-    return resultado
+from cifrado import cifrado_cesar
 
 if __name__ == '__main__':
     texto_original = input('Ingrese el texto a cifrar: ')
